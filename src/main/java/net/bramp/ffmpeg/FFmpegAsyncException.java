9acf12d8c957ea23c7412d4d620ee04fcca3958f
Joel Widmer <joel@widmer.me>
2024-08-11 11:17:04 +0200

    chore: add missing serialVersionUID

--- src/main/java/net/bramp/ffmpeg/FFmpegAsyncException.java
+++ src/main/java/net/bramp/ffmpeg/FFmpegAsyncException.java
@@ -1,6 +1,7 @@
 package net.bramp.ffmpeg;
 
 public class FFmpegAsyncException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
 
     public FFmpegAsyncException(Throwable cause) {
         super(cause);
